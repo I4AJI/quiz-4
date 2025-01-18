0f2269c57c5845bd1e9997b83e80d19f1f332e08
Max <maxbritish4@gmail.com>
2025-01-19 00:56:13 +0200

    Initial Express app setup

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${3000}`);
 });
+s
